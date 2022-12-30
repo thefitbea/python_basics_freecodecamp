dee858e6a018db4d6117c9308fc1d25483ac7d47
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2022-12-31 00:49:00 +0530

    Add files via upload

---
+++ amma.py
@@ -0,0 +1,55 @@
+#story of my life
+
+#variable decalrations
+
+my_name="Harikrishnan N Namboothiri"
+my_nick_name="Kannan"
+my_age_string="24"
+my_age=24
+is_male=True
+my_pseudo_name="Krish"
+my_dad_name="Narayanan"
+my_pet_name="Yuki"
+my_mummy_name="Chandrika"
+my_hobby="learning"
+absolute_value=-4.4
+
+#action sequence
+print("Hi\t"+my_mummy_name)
+
+print("Hi\n"+my_name)
+
+print("I'm\t"+my_age_string)
+print(my_hobby.upper()+"\tPYTHON".lower())
+print(len(my_hobby))
+print(my_hobby[0])
+print(my_hobby.index("n"))
+print(my_hobby.replace("learning","playing"))
+print(-4.4)
+
+print(str(my_age)+"\tis my age")
+print(abs(absolute_value))
+print(pow(3,2))
+print(max(4,16))
+print(min(3,98))
+print(round(3.8))
+
+#advanced math functions by importing
+
+from math import *#math module
+print(floor(6.8))
+print(ceil(6.8))#round off, exact opp of floor fn
+print(sqrt(100))
+
+#taking info from user
+
+villain_name=input("Enter Villain Name:")
+villain_age=input("Enter Villain's age:")
+print("Hello\t"+villain_name+"!, you are\t"+villain_age)
+
+
+
+
+
+
+
