86739ed19087b1c410e4ef5bd0c2b15600d47138
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2022-12-31 18:35:05 +0530

    Add files via upload

--- reading external file.py
+++ reading external file.py
@@ -7,10 +7,24 @@ print(file.readable())# checking if file can be read
 #print(file.readline())
 
 #print(file.readlines())# puts into list
-#print(file.readlines()[1])#doubt with error in index
+#print(file.readlines()[0])#doubt with error in index, fixed by Vinu Cutan
+#after first readline pointer reading list is set to last element and list is empty so throws error
+
 for line in file.readlines():
     print(line)
 
 file.close()
 
 #print(file)
+
+file=open("external_text_file.txt", "a")
+
+file.write("\nAppENDed text line")
+
+file.close()
+
+file=open("external_text_file_copy.txt","r+")#creates a new line re writes all content
+file.write("\n \n hahahaha Happy new gear, it ain't a typo")
+for text in file.readlines():
+    print(text)
+file.close()
